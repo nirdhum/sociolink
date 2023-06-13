bf4bcc5f7cea7ff2a95eb47aa3cd248a7e026c1d
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-06-13 14:49:14 +0530

    Added script for the font size change functionaliy in theme section

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const messageBox = document.querySelector('.messages')
 const messages = messageBox.querySelectorAll('.message')
 const messageSearch = document.getElementById('message-search')
 
+const theme = document.getElementById('theme')
+const themeBox = document.querySelector('.customize-theme')
+
+const fontSizes = document.querySelectorAll('.choose-size span')
+
+let rootEL = document.querySelector(':root')
+
+const colorPallet = document.querySelectorAll('.choose-color span')
+
 const removeCurrentActive = () => {
     menuItems.forEach(item => {
         item.classList.remove('active')
@@ -32,7 +41,7 @@ messageBtn.addEventListener('click', () => {
 
     setTimeout(() => {
         messageBox.style.boxShadow = 'none'
-    }, 3000)
+    }, 2000)
 })
 
 const searchMsg = () => {
@@ -50,3 +59,64 @@ const searchMsg = () => {
 }
 
 messageSearch.addEventListener('keyup', searchMsg)
+
+const openTheme = () => {
+    themeBox.style.display = "grid"
+}
+
+const closeTheme = (event) => {
+
+    if (event.target.classList.contains('customize-theme')) {
+        themeBox.style.display = 'none'
+    }
+
+}
+
+themeBox.addEventListener('click', closeTheme)
+
+theme.addEventListener('click', openTheme)
+
+const removeSize = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active')
+    })
+}
+
+fontSizes.forEach(size => {
+
+    size.addEventListener('click', () => {
+        removeSize()
+        let font_size;
+        size.classList.toggle('active')
+
+        if (size.classList.contains('font-size-1')) {
+            console.log('clicked');
+            font_size = '10px'
+            rootEL.style.setProperty('--sticky-top-left', '5.4rem')
+            rootEL.style.setProperty('--sticky - top - right', '5.4rem')
+
+        } else if (size.classList.contains('font-size-2')) {
+            font_size = '13px'
+            rootEL.style.setProperty('--sticky-top-left', '5.4rem')
+            rootEL.style.setProperty('--sticky - top - right', '-7rem')
+
+        } else if (size.classList.contains('font-size-3')) {
+            font_size = '16px'
+            rootEL.style.setProperty('--sticky-top-left', '-2rem')
+            rootEL.style.setProperty('--sticky - top - right', '-17rem')
+
+        } else if (size.classList.contains('font-size-4')) {
+            font_size = '19px'
+            rootEL.style.setProperty('--sticky-top-left', '-5rem')
+            rootEL.style.setProperty('--sticky - top - right', '-25rem')
+
+        } else if (size.classList.contains('font-size-5')) {
+            font_size = '22px'
+            rootEL.style.setProperty('--sticky-top-left', '-10rem')
+            rootEL.style.setProperty('--sticky - top - right', '-33rem')
+
+        }
+
+        document.querySelector('html').style.fontSize = font_size
+    })
+})
